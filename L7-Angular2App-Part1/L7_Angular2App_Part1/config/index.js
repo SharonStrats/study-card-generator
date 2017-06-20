83cb57f72e7c786a863b9674d238b499907853e5
Johanes Riandy <johanesriandy@gmail.com>
2017-06-20 12:33:17 +0800

    Delete unecessary folders and files

--- L7-Angular2App-Part1/L7_Angular2App_Part1/config/index.js
+++
@@ -1,9 +0,0 @@
-var configValues = require('./config');
-
-module.exports = {
-    
-    getDbConnectionString: function() {
-        return 'mongodb://' + configValues.uname + ':' + configValues.pwd + '@ds015849.mlab.com:15849/nodetodosample';
-    }
-    
-}
